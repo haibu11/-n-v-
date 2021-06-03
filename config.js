deff0569202f9221e15cb40d0fbbf9d43ace71d3
haibu11 <85263008+haibu11@users.noreply.github.com>
2021-06-03 07:15:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,11 @@
 const CONFIG = {
     introTitle: 'Cậu ơi!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'thích cậu mà <33',
     btnNo: 'Không, bạn bè thui',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu tớ <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰',
